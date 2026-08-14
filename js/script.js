b90a15fe756579a223a0c95e5d899ae28d572e01
RayalissonP <rayrpm@hotmail.com>
2026-08-14 00:25:09 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-function limparFormulario(){
-  if(confirm("Tem certeza que deseja limpar todos os campos do formulário?")){
-    document.getElementById('formAvaliacao').reset();
-    sincronizarRodape();
-  }
-}
-
 // Mantém o rodapé da página 2 sempre igual ao da página 1 
 function sincronizarRodape(){
   var origem = document.getElementById('endereco_instituto');
